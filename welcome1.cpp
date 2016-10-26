c9776b0b9a3b581aa3392aed91ee48fbe6ddc25e
unknown <>
2016-10-26 14:08:40 +0800

    changes log

---
+++ welcome1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void)
+{
+  cout<<"Hello,this is the second test program on GitHub "<<endl;
+  system("pause");
+  return 0;
+}
